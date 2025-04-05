e93bcf63ef944b59f868cb88e328dabcb7ba1727
Julio Mena <layoutlinux@protonmail.com>
2025-04-05 08:24:16 +0100

    Modified and added some keys

--- lua/usr/core/keymaps.lua
+++ lua/usr/core/keymaps.lua
@@ -1,27 +1,41 @@
-local opts = { noremap = true, silent = true }
 
 -- Leader key
 vim.g.mapleader = " "
+vim.g.locamaplleader = " "
 
 -- Basic keymaps
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, opts) -- open netrw
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts) -- move line down
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts) -- move line up
-vim.keymap.set("n", "<leader>s", ":w<CR>", opts)    -- save file
+--vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {noremap = true, silent = true, desc = "open netrw"})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv",  {noremap = true, silent = true, desc = "move line down"})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv",  {noremap = true, silent = true, desc = "move line up"})
+vim.keymap.set("n", "<leader>s", ":w<CR>",  {noremap = true, silent = true, desc = "save file"})
 
 -- Copy/paste/cut
-vim.keymap.set("n", "<leader>y", '"+y', opts)
-vim.keymap.set("v", "<leader>y", '"+y', opts)
-vim.keymap.set("n", "<leader>d", '"+d', opts)
-vim.keymap.set("v", "<leader>d", '"+d', opts)
-vim.keymap.set("n", "<leader>p", '"+p', opts)
-vim.keymap.set("v", "<leader>p", '"+p', opts)
-
--- Navigation (consider that < and > may need to be adapted)
-vim.keymap.set("n", "<C-n>", ":tabn<CR>", opts)
-vim.keymap.set("n", "<C-p>", ":tabp<CR>", opts)
+vim.keymap.set("n", "<leader>y", '"+y',  {noremap = true, silent = true, desc = "copy on normal mode"})
+vim.keymap.set("v", "<leader>y", '"+y',  {noremap = true, silent = true, desc = "copy on visual mode"})
+vim.keymap.set("n", "<leader>d", '"+d',  {noremap = true, silent = true, desc = "cut on normal mode"})
+vim.keymap.set("v", "<leader>d", '"+d',  {noremap = true, silent = true, desc = "cut on visual mode"})
+vim.keymap.set("n", "<leader>p", '"+p',  {noremap = true, silent = true, desc = "paste on normal mode"})
+vim.keymap.set("v", "<leader>p", '"+p',  {noremap = true, silent = true, desc = "paste on visual mode"})
 
 -- Visual Block --
-vim.keymap.set("v", "V", "^vg_", opts)
+vim.keymap.set("v", "V", "^vg_",  {noremap = true, silent = true, desc = "change to v-line"})
+
+-- Tabs
+vim.keymap.set("n", "<C-t>", ":tabe<CR>", {noremap = true, silent = true, desc = "open tab page"})
+vim.keymap.set("n", "<C-p>", ":-tabe<CR>", {noremap = true, silent = true, desc = "opens tabpage before the current"})
+vim.keymap.set("n", "<C-n>", ":+tabe<CR>", {noremap = true, silent = true, desc = "tabpage after the next tab"})
+
+vim.keymap.set("n", "<C-c>", ":tabc<CR>", {noremap = true, silent = true, desc = "close the current tab page"})
+vim.keymap.set("n", "<C-z>", ":-tabc<CR>", {noremap = true, silent = true, desc = "close the previous tab page"})
+vim.keymap.set("n", "<C-x>", ":+tabc<CR>", {noremap = true, silent = true, desc = "close the next tab page"})
+vim.keymap.set("n", "<C-q>", ":tabo<CR>", {noremap = true, silent = true, desc = "close all tab pages except the current one"})
+
+vim.keymap.set("n", "<C-Left>", ":-tabn<CR>", {noremap = true, silent = true, desc = "go to the previous tab page"})
+vim.keymap.set("n", "<C-Right>", ":+tabn<CR>", {noremap = true, silent = true, desc = "go to the next tab page"})
+
+vim.keymap.set("n", "<C-A-0>", ":0tabm<CR>", {noremap = true, silent = true, desc = "move the tab page to the first"})
+vim.keymap.set("n", "<C-A-Left>", ":-tabm<CR>", {noremap = true, silent = true, desc = "move the tab page to the left"})
+vim.keymap.set("n", "<C-A-Right>", ":+tabm<CR>", {noremap = true, silent = true, desc = "move the tab page to the right"})
 
 
+vim.keymap.set("n", "<leader>fz", ":FzfLua files<CR>", {noremap = true, silent = true, desc = "fzf files"})
